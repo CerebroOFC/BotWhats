2e7aba7666c0fbddc438a13d5e1318905447c993
XCoders Company <blackhatscentral@gmail.com>
2021-10-06 14:03:51 -0300

    meu primeiro commit

---
+++ src/help.js
@@ -0,0 +1,71 @@
+const help = (prefix) => {
+	return `
+
+BOT CRIADO PELA EXIPE XCODERS COMPANY
+- ME CRIARAM SÓ PRA ENXER MEU SACO
+	
+*COMANDOS PRA FIGURINHAS:*
+
+Comandos: *${prefix}sticker* ou *${prefix}stiker*, ve se não digita errado burro.
+Desc: converter imagem / gif / vídeo em figurinha
+Uso: Enviar ou responder imagem/gif/vídeo com legenda a *${prefix}sticker*\n
+Comando : *${prefix}toimg*
+Desc: Converte uma figurinha em imagem
+Uso: Responda a figurinha com *${prefix}toimg*\n
+
+*COMANDOS PARA USAR EM GRUPOS, VE SE NÃO USA MUITO PQ NÃO SOU SEU ESCRAVO:*
+
+Comando : *${prefix}welcome [1/0]*
+Desc: Ativa o mode de boas vinda para as puta que entrarem no grupo
+Uso: * ${prefix}bemvindo 1 ou 0* \n
+Comando : *${prefix}add*
+Desc: Adciona membro ao grupo
+Uso: *${prefix} add 62813xxxxx* \n
+Observação: Só pode ser usado se o bot for administrador do grupo, quer ser admin ? tem que dar a bundinha \n
+Comando : *${prefix}kick*
+Desc: Expulsa membro do grupo
+Uso: *${prefix} kick @[membro do grupo]* \n
+Observação: Só pode ser usado se o bot for administrador do grupo, tu é admin seu bosta ?  \n
+Comando : *${prefix}promote*
+Desc: Torna um membro administrador do grupo
+Observação: Só pode ser usado se o bot for administrador do grupo, quer adicionar como seu merda, nem admin tu é  \n
+Uso: *${prefix}promote @[numero do membro]* \n
+Observação: Só pode ser usado se o bot for administrador do grupo, nem tu é admin, vai deixar outros como admin como ? \ n
+Comando : *${prefix}demote*
+Desc: Retirar cargo de administrador do grupo
+Uso: *${prefix}demote @[mumero do membro]* \n
+Observação: Só pode ser usado se o bot for administrador do grupo \n
+Comando : *${prefix}linkgroup*
+Desc: Retorna o link de convite do grupo dos gado
+Uso: Basta enviar o comando
+Comando : *${prefix}leave*
+Desc: Use este comando para o bot sair do grupo
+Uso: Basta enviar o comando
+Nota: Só pode ser usado por administradores do grupo ou o dono no bot, que cara burro mano, posso tirar esse cara ?\n
+Comando : *${prefix}tagall*
+Desc: Marca todos os membros viadinhos do grupo, inclusive administradores
+Uso: Basta enviar o comando
+Nota: Este comando só pode ser usado se você for administrador do grupo, se nao for vaza comédia!
+Comando : *${prefix}tagall2*
+Desc: Marca todos os membros viadinhos do grupo, inclusive administradores
+Uso: Basta enviar o comando
+Nota: Este comando só pode ser usado se você for administrador do grupo, se nao for vaza comédia!
+comando : *${prefix}tagall3*
+Desc: Marca todos os membros viadinhos do grupo, inclusive administradores
+Uso: Basta enviar o comando
+Nota: Este comando só pode ser usado se você for administrador do grupo, se nao for vaza comédia!
+
+*PARA O DONO DO BOT:*
+
+Comando : *${prefix}bc*
+Desc: Transmissão
+Uso: *${prefix}bc [texto] * \nexemplo: *${prefix}bc sua mensagem*
+Nota: Este comando só pode ser usado pelo proprietário do bot \n
+Comando : *${prefix}setprefix*
+Desc: substituir prefixo
+Uso: *${prefix} setprefix [texto | opcional] * \ nexemplo: * $ {prefix}setprefix ?*
+Nota: Este comando só pode ser usado pelo proprietário do bot \n\n
+`
+}
+
+exports.help = help
